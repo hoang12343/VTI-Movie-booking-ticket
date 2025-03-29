930294ff41765e0240e8bd2613015fdd336baf00
hoang12343 <hoanggg322004@gmail.com>
2025-03-29 10:09:49 +0700

    test

---
+++ src/main/java/com/example/vti_movie_booking_ticket/VtiMovieBookingTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.vti_movie_booking_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtiMovieBookingTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtiMovieBookingTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vti_movie_booking_ticket/VtiMovieBookingTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vti_movie_booking_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtiMovieBookingTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
